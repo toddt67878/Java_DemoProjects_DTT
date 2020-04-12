8dfcde860ae2bb9f238db7892e4929543c6d408f
Todd Traylor <toddt67878@yahoo.com>
2020-04-11 21:56:54 -0500

    Second Commit

---
+++ Java DemoProjects/primitive.java
@@ -0,0 +1,13 @@
+public class Primitive {
+    public static void main( String[] args ) {
+        int x;
+        int y;
+        int z;
+
+        y = 7;
+        z = 4;
+        x = y + z;
+
+        System.out.println( "x = " + x );
+    }
+}
